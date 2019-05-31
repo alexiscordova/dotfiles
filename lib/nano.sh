9e160b4045aac22cda96c03b1a45439b839ac9bc
Alexis Córdova <acordova@mac.com>
2019-05-30 21:36:34 -0700

    Write to file in home directory

--- lib/nano.sh
+++ lib/nano.sh
@@ -10,7 +10,7 @@ curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
 ## Configure nanorc
 echo 'Configuring nanorc'
-printf '\n' >> .nanorc
-printf 'set softwrap' >> .nanorc
-printf 'set tabsize 2' >> .nanorc
-printf 'set tabstospaces' >> .nanorc
+printf '\n' >> ~/.nanorc
+printf 'set softwrap' >> ~/.nanorc
+printf 'set tabsize 2' >> ~/.nanorc
+printf 'set tabstospaces' >> ~/.nanorc
