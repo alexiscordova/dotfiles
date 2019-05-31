3e6f127645f69b93289755ae5cac8dc30f9963dc
Alexis Córdova <acordova@mac.com>
2019-05-30 23:17:40 -0700

    Add load NVM export

--- lib/nodejs.sh
+++ lib/nodejs.sh
@@ -7,6 +7,11 @@
 # Install and configure NVM
 echo '📦 Installing NVM'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+echo '📦 Load NVM'
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 echo '📦 Configuring Node.js'
 nvm install --lts
 nvm use node
